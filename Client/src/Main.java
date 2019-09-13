8f843221fefbffec0af9bcf911771e532f6c21ac
Thienng97 <duc-thien@sympatico.ca>
2019-09-13 00:07:52 -0400

    inital commit

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -15,31 +15,5 @@ public class Main {
 		clientDisplay.startConsole();
 		
 	}
-	
-//	private static void instanciateSocket() {
-//		Socket clientSocket = null;
-//		String IP_ADDRESS = "10.200.27.199";
-//		System.out.println("test");
-//		try {
-//			try {
-//				clientSocket = new Socket(IP_ADDRESS, 5000);
-//				
-//				ObjectOutputStream objectOutput = new ObjectOutputStream(clientSocket.getOutputStream());
-//				
-//				objectOutput.writeObject(new String("PENIS BANDE"));
-//				objectOutput.flush();
-//				
-//				ObjectInputStream obj = new ObjectInputStream(clientSocket.getInputStream());
-//				
-//				System.out.println("receiving " + obj.readObject());
-//				
-//			} finally {
-//				clientSocket.close();
-//			}
-//			
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//		}
-//	}
 
 }
