1bff41ed794a822ebfb90a9714fabaf9f58db95a
Jeremy Boulet <jeremyboulet85@gmail.com>
2019-10-20 19:47:27 -0400

    dfg

--- Client/src/ClientDisplay.java
+++ Client/src/ClientDisplay.java
@@ -143,7 +143,7 @@ public class ClientDisplay {
 			int totalRead = 0;
 			int remaining = fileSize;
 			
-			while((read = objectInput.read(bytes, 0, Math.min(bytes.length, remaining))) != -1 ) {
+			while((read = objectInput.read(bytes, 0, Math.min(bytes.length, remaining))) > 0 ) {
 				totalRead += read;
 				remaining -= read;
 				fos.write(bytes, 0 , read);
