5595cba3bdcd2e889db6f344db7f66b156c086eb
Jeremy Boulet <jeremyboulet85@gmail.com>
2019-10-20 20:39:51 -0400

    dfds

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
 		ClientDisplay clientDisplay = new ClientDisplay();
 		clientDisplay.getInformations();
